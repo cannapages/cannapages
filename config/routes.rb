ce174d93e2ddc30677dbc15047f9142ba9c50463
tyrel86 <tyrelrichey@gmail.com>
2013-01-25 11:29:52 -0700

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,63 +1,14 @@
 C41::Application.routes.draw do
-  get "forums/index"
-
-  get "forums/show"
-
-  get "forums/edit"
-
-  get "forums/new"
-
-  get "rss_feeds/index"
-
-  get "rss_feeds/admin_index"
-
-  get "rss_feeds/new"
-
-  get "rss_feeds/edit"
-
-  get "strains/index"
-
-  get "strains/show"
-
-  get "strains/new"
-
-  get "strains/edit"
-
-  get "critiques/index"
-
-  get "critiques/show"
-
-  get "critiques/new"
-
-  get "critiques/edit"
-
-  get "articles/index"
-
-  get "articles/show"
-
-  get "articles/new"
-
-  get "articles/edit"
-
-  get "listings/index"
-
-  get "listings/show"
-
-  get "listings/new"
-
-  get "listings/edit"
-
-  get "listings/admin_index"
-
-  get "listings/admin_edit"
-
-  get "listings/admin_new"
-
-  get "home/index"
-  get "home/critiques"
-
 	root :to => "home#index"
 	devise_for :users
-
 	resources :searches
+
+	get "/listings/admin_index" => "listings#admin_index", as: :listings_admin_index
+	get "/listings/:id/admin_edit" => "listings#admin_edit", as: :admin_edit_listing
+	put "/listings/:id/admin" => "listings#admin_update", as: :admin_update_listing
+	get "/listings/admin_new" => "listings#admin_new", as: :admin_new_listing
+	post "/listings/admin" => "listings#admin_create", as: :admin_create_listing
+	get "/searches/admin_index" => "controller#action", as: "searchs_admin_index"
+	resources :listings
+
 end

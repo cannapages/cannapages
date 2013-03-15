1243cf9dd8ef86943942e86448a659dce3e6d47a
tyrel86 <tyrelrichey@gmail.com>
2013-03-14 18:15:35 -0600

    JKL

--- app/assets/javascripts/my_leaflet.js
+++ app/assets/javascripts/my_leaflet.js
@@ -1,4 +1,7 @@
+var map;
+var markers = [];
 $(document).ready( function() {
+
 	var user_location = $('user_location')
 	var user_lat = parseInt(user_location.data('lat'))
 	var user_lng = parseInt(user_location.data('lng'))
@@ -13,18 +16,33 @@ $(document).ready( function() {
 			value = $(value)
 			lat = value.data('lat')
 			lng = value.data('lng')
-			
+			if( lat > max_lat ) {
+				max_lat = lat
+			}			
+			if( lat < min_lat ) {
+				min_lat = lat
+			}			
+			if( lng > max_lng ) {
+				max_lng = lng
+			}			
+			if( lng < min_lng ) {
+				min_lng = lng
+			}
 		})
 
 		map = new L.Map("map", {
       center: [user_lat, user_lng],
-      zoom: 12
+      zoom: 14
     })
 		main_layer = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png', {
 			key: '5990419e5e434f22be45a5f491ce3e54',
-			styleId: 89663,
+			styleId: 89688,
 		})
 		map.addLayer( main_layer )
+		var southWest = new L.LatLng(min_lat, min_lng),
+    		northEast = new L.LatLng(max_lat, max_lng),
+    		bounds = new L.LatLngBounds(southWest, northEast);
+		map.fitBounds( bounds )
 
 		mainIcon = L.icon({
 				iconUrl: '/assets/marker-icon.png',

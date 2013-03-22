5a19f84f4695ebc8ae0f04d6106c26c711f3288b
tyrel86 <tyrelrichey@gmail.com>
2013-03-21 20:52:57 -0600

    JKL

--- app/models/volume.rb
+++ app/models/volume.rb
@@ -5,11 +5,36 @@ class Volume
 
 	field :volume_number, type: Integer
 	field :live, type: Boolean
+	field :column1, type: Array
+	field :column2, type: Array
+	field :column3, type: Array
   has_mongoid_attached_file :volume_image, :styles => { :large => "300x300#", :small => "150x150#" }
 
-	has_many :articles
-
 	before_create :set_volume_number
+
+	def add_to_column(	col_num, element )
+		eval("self.column#{col_num} ||= []")
+		eval("self.column#{col_num} += [{slug: '#{element.slug}', element_class: '#{element.class}'}]")
+	end
+
+	def get_3_col_array
+		self.column1 ||= []
+		self.column2 ||= []
+		self.column3 ||= []
+		col1 = []
+		col2 = []
+		col3 = []
+		self.column1.each do |element|
+			col1 << eval("#{element['element_class']}.find_by( slug: \"#{element['slug']}\" )")
+		end
+		self.column2.each do |element|
+			col2 << eval("#{element['element_class']}.find_by( slug: \"#{element['slug']}\" )")
+		end
+		self.column3.each do |element|
+			col3 << eval("#{element['element_class']}.find_by( slug: \"#{element['slug']}\" )")
+		end
+		[col1,col2,col3]
+	end
 
 	def set_volume_number
 		last_volume = Volume.order_by( created_at: :desc ).first

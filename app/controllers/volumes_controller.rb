25e89925b8e63909cda3890ce9cdc9888c615619
tyrel86 <tyrelrichey@gmail.com>
2013-03-22 14:43:49 -0600

    JKL

--- app/controllers/volumes_controller.rb
+++ app/controllers/volumes_controller.rb
@@ -5,4 +5,19 @@ class VolumesController < ApplicationController
 		@volumes = Volume.all.to_a
 		render layout: "admin_backend"
 	end
+
+	def edit
+		@volume = Volume.find( params[:id] )
+		render layout: "admin_backend"
+	end
+
+	def update
+		@volume = Volume.find( params[:id] )
+		@volume.add_to_column( params[:col_num], Article.find_by( slug: params[:article]) )
+		if @volume.save
+			redirect_to edit_volume_path @volume
+		else
+			render "edit"
+		end
+	end
 end

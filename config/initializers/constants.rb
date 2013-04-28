893169da29beb971d0756b503fd0f7fd8d5d4d0b
tyrel86 <tyrelrichey@gmail.com>
2013-04-28 11:27:00 -0600

    JKL

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,7 +1,7 @@
 LEGAL_STATE_ARRAY = [ 'Alaska', 'Arizona', 'California', 'Colorado', 
                       'Connecticut', 'DC', 'Delaware', 'Hawaii, Maine', 'Michigan', 
                       'Montana', 'Initiative', 'Nevada', 'New Jersey', 'New Mexico', 
-                      'Massachusetts', 'Oregon', 'Rhode Island' ] 
+                      'Massachusetts', 'Oregon', 'Rhode Island', 'Washington' ] 
                                         
 YESNO = ['yes','no']           
   
